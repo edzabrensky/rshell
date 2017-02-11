91c971c54b1e69dacfc4eaca2d8191ae4a1d4753
Edward Zabrensky <ezabr001@hammer.cs.ucr.edu>
2017-02-11 14:18:23 -0800

    Finished the extra credit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include <unistd.h>
 #include "Expression.h"
 using namespace std;
 int main() {
 	string s;
 	Expression * e;
+	char hostname[1024];
+	int result = gethostname(hostname, 1024);
+	char username[1024];
+	result = getlogin_r(username, 1024);
 	while(1) {
+		printf("%s@%s", username, hostname);
 		cout << "$ ";
 		getline(cin, s);
 		e = new Expression(s);
