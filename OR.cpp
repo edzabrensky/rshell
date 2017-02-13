9dd4b7718ab97f3b2276d2045a51c38383473d4f
cchin013 <cchin013@ucr.edu>
2017-02-12 16:23:49 -0800

    Update OR.cpp

--- OR.cpp
+++ OR.cpp
@@ -1,1 +1,15 @@
+#include "OR.h"
 
+int OR::runCommand(string cmd)
+{
+  Connector::runCommand(cmd);
+  if (success)
+  {
+    return 2;
+  }
+  if (!success)
+  {
+    return 1;
+  }
+  return 1;
+}
