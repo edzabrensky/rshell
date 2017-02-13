c57090a9ed2d4ce710c8863a19f12dedfd0d96ec
cchin013 <cchin013@ucr.edu>
2017-02-12 19:42:10 -0800

    pid and execvp initial set up

--- Connector.cpp
+++ Connector.cpp
@@ -1,19 +1,37 @@
 #include "Connector.h"
-
 Connector::Connector() {
 	
 }
 
 int Connector::runCommand(string cmd) {
 	//syscall on cmd; fork, execvp, waitpid
-	if (worked)
+	pid_t pid = fork();
+	
+	if (pid == -1) //error
 	{
-		this->success = true;
-	}
-	if (!worked)
-	{
+		perror ("fork");
 		this->success = false;
 	}
+		
+	if (pid == 0) //child
+	{
+		this->success = true;
+		if ( execvp(REPLACE WITH PARAMETERS) == -1 ) //ADD PARAMETERS FOR EXECVP
+		{
+			perror ("exec");
+		}
+	}
+	
+	if (pid > 0) //parent
+	{
+		if (wait(0) == -1)
+		{
+			perror ("wait");
+		}
+			
+	}
+	
+	
 	return 1;
 }
 
