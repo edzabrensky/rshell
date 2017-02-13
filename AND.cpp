90b2591206902b20c053f761dfe32fb3b4c5caa4
cchin013 <cchin013@ucr.edu>
2017-02-12 16:21:37 -0800

    Update AND.cpp

--- AND.cpp
+++ AND.cpp
@@ -1,1 +1,15 @@
 #include "AND.h"
+
+int AND::runCommand(string cmd)
+{
+  Connector::runCommand(cmd);
+  if (success)
+  {
+    return 1;
+  }
+  if (!success)
+  {
+    return 2;
+  }
+  return 2;
+}
