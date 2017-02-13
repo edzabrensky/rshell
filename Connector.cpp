92a4769a98c7d85e46234e8e78be2f97b7985c68
cchin013 <cchin013@ucr.edu>
2017-02-12 16:41:11 -0800

    pseudocode added

--- Connector.cpp
+++ Connector.cpp
@@ -5,6 +5,15 @@ Connector::Connector() {
 }
 
 int Connector::runCommand(string cmd) {
+	//syscall on cmd; fork, execvp, waitpid
+	if (worked)
+	{
+		this->success = true;
+	}
+	if (!worked)
+	{
+		this->success = false;
+	}
 	return 1;
 }
 
