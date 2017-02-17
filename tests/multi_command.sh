0234c84806d9dc5781f236b4e1b708a77533b68b
cchin013 <cchin013@ucr.edu>
2017-02-17 15:46:18 -0800

    added more tests

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,2 +1,2 @@
 #tests commands with ;, &&, or
-ls -a; echo hello && mkdir test || echo world; git status
+ls -a; echo hello && mkdir test || echo world; git status && echo hello || echo world || ls -l || touch multicommandtest && ls; echo a ||echo b||echo c|| echo hello&&ls -l;
