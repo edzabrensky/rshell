9b3f91513eaf754cce52a6c367a5f7ba17f5a6b5
cchin013 <cchin013@ucr.edu>
2017-02-17 18:57:15 -0800

    finished multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -6,7 +6,7 @@ echo world || emptydir && git status
 echo 1; echo 2; echo 3; echo 4; echo 5 && echo 6 || echo 7 || echo 8; echo 9 && echo 10 && echo 11
 ls -a; echo hello && mkdir test || echo world; git status && echo hello || echo world || ls -l || touch multicommandtest && ls; echo a ||echo b||echo c|| echo hello&&ls -l;
 ls; rm -r test && rm multicommandtest
-echo test || echo world
-echo world || echo test
-echo test && echo world
+helloxD123 || echo world
+echo world || helloxD123
+echo test && echo world; ls -l
 echo world && echo test
