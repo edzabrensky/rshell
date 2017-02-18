5f3a8ccbc4a2bf3c462bacc057de5cd0ded3770e
cchin013 <cchin013@ucr.edu>
2017-02-17 16:00:16 -0800

    added more tests

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,4 +1,11 @@
 #tests commands with comments
-ls -a #echo comment
-echo hello #ls -l
-echo world #mkdir test
+ls -a #echo comment###ls -l
+echo hello #ls -l #ls -a #ls #ls######echo world
+echo world #mkdir test #exit
+ls -l #echo test #mkdir folder
+mkdir testdir #exit #echo 123
+echo this is a series of words that print that should cut off here #and not continue to print to the console
+#echo this should not print
+#echo this should also not print
+echo creating an empty text file named emptytext #touch emptytextShouldNotBeMade
+touch emptytext #echo world && echo hello || mkdir empty || ls -l && echo done || exit;
