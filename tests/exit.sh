ac1976c39d6d6f80685400df1a8b6d6f26837c7e
cchin013 <cchin013@ucr.edu>
2017-02-17 16:10:57 -0800

    added more tests

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,3 +1,6 @@
 #tests exit and commands with exit
-ls -a; echo hello; exit
-exit;
+ls -a; echo hello
+echo world; invalidbashcommand && exit
+echo 123 #exit
+touch testfile; stillshouldntexit && exit
+ls -l; echo should exit || exit
