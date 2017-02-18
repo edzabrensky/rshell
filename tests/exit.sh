a160559db9a4f0db197a706d4aa8ddc68e05c502
cchin013 <cchin013@ucr.edu>
2017-02-17 16:16:05 -0800

    Update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,6 +1,11 @@
 #tests exit and commands with exit
 ls -a; echo hello
-echo world; invalidbashcommand && exit
+echo world; invalidbashcommand && exit #shouldn't exit since invalidbashcommand isn't a valid command
 echo 123 #exit
-touch testfile; stillshouldntexit && exit
-ls -l; echo should exit || exit
+touch testfile; stillshouldntexit && exit #shouldn't exit since stillshouldntexit isn't a valid command
+echo 456; echo dontexit || exit #shouldn't exit as echo dontexit should work
+ls -l; echo should exit && exit #should exit here as echo is a valid command
+echo hello #this part should not output
+echo 1 #none of these
+echo 2 #should print
+echo 3 #to console
